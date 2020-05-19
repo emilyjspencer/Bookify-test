cf840a05b1b6194479af29c1f30df569ca92273b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 14:02:21 +0100

    Creates a form to submit book details

--- bookify/src/components/create_book.js
+++ bookify/src/components/create_book.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class CreateBook extends Component {
     render() {
-        return (
-            <div>
-              <h1>Create Book component</h1>
-              </div>
-        );
+        return (<div class = "form-wrapper">
+             <Form>
+                 <Form.Group controlId="Title">
+                     <Form.Label>Title</Form.Label>
+                     <Form.Control type="text"/>
+                 </Form.Group>
+        
+             <Form.Group controlId="Author">
+                 <Form.Label>Author</Form.Label>
+                 <Form.Control type="text"/>
+             </Form.Group>
+
+             <Form.Group controlId="Genre">
+                 <Form.Label>Genre</Form.Label>
+                 <Form.Control type="text" />
+             </Form.Group>
+
+             <Button variant="info" size="lg" block="block" type="submit">
+                Create Book
+            </Button>
+            </Form>
+            </div>);   
     }
 }
 
