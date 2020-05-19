54cac1f32835cead2cb14480490cb60a5990b24b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 13:35:27 +0100

    Adds Editbook component

---
+++ bookify/src/components/edit_book.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditBook extends Component {
+    render() {
+        return (
+            <div>
+              <p>Edit Book component</p>
+              </div>
+        );
+    }
+}
+
+export default EditBook;
