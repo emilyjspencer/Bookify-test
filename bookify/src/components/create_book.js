c42e5df6a65e512396227a05405766dedf1878fa
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 14:25:51 +0100

    Sets initial state - books have no name, genre or author. Defines a submit event to allow books to be created upon clicking the button. Creates event handlers

--- bookify/src/components/create_book.js
+++ bookify/src/components/create_book.js
@@ -3,6 +3,45 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 class CreateBook extends Component {
+    constructor(props) {
+      super(props)
+
+      this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+      this.onChangeauthor = this.onChangeAuthor.bind(this);
+      this.onChangeGenre = this.onChangeGenre.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+
+      this.state = {
+        title: '',
+        author: '',
+        genre: ''
+      }
+    }
+
+    onChangeBookTitle(e) {
+      this.setState({booktitle: e.target.value})
+    }
+
+    onChangeAuthor(e) {
+      this.setState({author: e.target.value})
+    }
+
+    onChangeGenre(e) {
+      this.setState({genre: e.target.value})
+    }
+
+    onSubmit(e) {
+      e.preventDefault()
+
+      console.log(`Book was successfully created`);
+      console.log(`BookTitle: ${this.state.booktitle}`);
+      console.log(`Author ${this.state.author}`);
+      console.log(`Genre: ${this.state.genre}`);
+    
+
+    this.setState({booktitle: '', author: '', genre: '' })
+ }
+
     render() {
         return (<div class = "form-wrapper">
              <Form>
