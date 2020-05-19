0c34db088a0879c094a42c733adce3259a8f387c
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 13:34:39 +0100

    Adds CreateBook component

---
+++ bookify/src/components/create_book.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateBook extends Component {
+    render() {
+        return (
+            <div>
+              <p>Create Book component</p>
+              </div>
+        );
+    }
+}
+
+export default CreateBook;
