f99140a653f8e84ef553c0aa59865fd697128606
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 13:36:06 +0100

    Adds BookList component

---
+++ bookify/src/components/book_list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return (
+            <div>
+              <p>Book List component</p>
+              </div>
+        );
+    }
+}
+
+export default CreateBook;
