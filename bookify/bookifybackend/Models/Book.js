3f3bbde2c59c62b8e5fadbae319ab6f0366b96b4
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-19 15:17:45 +0100

    Declares mongoDB database with schema, allowing for title, author and genre fields

---
+++ bookify/bookifybackend/Models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let bookSchema = new Schema({
+  booktitle: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  genre: {
+    type: String
+  }
+}, {
+    collection: 'books'
+  })
+
+module.exports = mongoose.model('Book', studentSchema)
